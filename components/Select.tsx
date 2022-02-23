da0c4f555ffe5a10885c079305f900183a3d2a13
aarnadlr <aaronadler@gmail.com>
2022-02-22 21:18:25 -0700

    Add Select options. Add form.

--- components/Select.tsx
+++ components/Select.tsx
@@ -2,50 +2,77 @@ import React from 'react';
 
 type Props = {
   label: string;
+  setStateFunction: (value: number) => void;
+  stateValue: number;
 };
 
-export default function Select({ label }: Props) {
+export default function Select({ label, setStateFunction, stateValue }: Props) {
   return (
     <div className="flex flex-col my-3">
+
       <label className="text-gray-500 text-sm" htmlFor={label}>
         {label}
       </label>
+
       <div className="flex">
         <select
           className="max-w-fit bg-gray-200 p-1.5 mr-1.5"
           name={label}
           id={label}
+          value={stateValue}
+          onChange={(e) => setStateFunction(Number(e.target.value))}
         >
-          <option value="12:00">12:00</option>
-          <option value="12:30">12:30</option>
-          <option value="1:00">1:00</option>
-          <option value="1:30">1:30</option>
-          <option value="2:00">2:00</option>
-          <option value="2:30">2:30</option>
-          <option value="3:00">3:00</option>
-          <option value="3:30">3:30</option>
-          <option value="4:00">4:00</option>
-          <option value="4:30">4:30</option>
-          <option value="5:00">5:00</option>
-          <option value="5:30">5:30</option>
-          <option value="5:00">6:00</option>
-          <option value="5:30">6:30</option>
-          <option value="5:00">7:00</option>
-          <option value="5:30">7:30</option>
-          <option value="5:00">8:00</option>
-          <option value="5:30">8:30</option>
-          <option value="5:00">9:00</option>
-          <option value="5:30">9:30</option>
-          <option value="5:00">10:00</option>
-          <option value="5:30">10:30</option>
-          <option value="5:00">11:00</option>
-          <option value="5:30">11:30</option>
+          <option value="0">0 mins</option>
+          <option value="30">30 mins</option>
+          <option value="60">1 hour</option>
+          <option value="90">1.5 hours</option>
+          <option value="120">2 hours</option>
+          <option value="150">2.5 hours</option>
+          <option value="180">3 hours</option>
+          <option value="210">3.5 hours</option>
+          <option value="240">4 hours</option>
+          <option value="270">4.5 hours</option>
+          <option value="300">5 hours</option>
+          <option value="330">5.5 hours</option>
+          <option value="360">6 hours</option>
+          <option value="390">6.5 hours</option>
+          <option value="420">7 hours</option>
+          <option value="450">7.5 hours</option>
+          <option value="480">8 hours</option>
+          <option value="510">8.5 hours</option>
+          <option value="540">9 hours</option>
+          <option value="570">9.5 hours</option>
+          <option value="600">10 hours</option>
+          <option value="630">10.5 hours</option>
+          <option value="660">11 hours</option>
+          <option value="690">11.5 hours</option>
+          <option value="720">12 hours</option>
+          <option value="750">12.5 hours</option>
+          <option value="780">13 hours</option>
+          <option value="810">13.5 hours</option>
+          <option value="840">14 hours</option>
+          <option value="870">14.5 hours</option>
+          <option value="900">15 hours</option>
+          <option value="930">15.5 hours</option>
+          <option value="960">16 hours</option>
+          <option value="990">16.5 hours</option>
+          <option value="1020">17 hours</option>
+          <option value="1050">17.5 hours</option>
+          <option value="1080">18 hours</option>
+          <option value="1110">18.5 hours</option>
+          <option value="1140">19 hours</option>
+          <option value="1170">19.5 hours</option>
+          <option value="1200">20 hours</option>
+          <option value="1230">20.5 hours</option>
+          <option value="1260">21 hours</option>
+          <option value="1290">21.5 hours</option>
+          <option value="1320">22 hours</option>
+          <option value="1350">22.5 hours</option>
+          <option value="1380">23 hours</option>
+          <option value="1410">23.5 hours</option>
+          <option value="1440">24 hours</option>
         </select>
 
-        <select className="max-w-fit bg-gray-200 p-1.5" name="ampm" id="ampm">
-          <option value="am">am</option>
-          <option value="pm">pm</option>
-        </select>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,18 @@ import Head from 'next/head';
 // import Image from 'next/image';
 import Select from '../components/Select';
 import styles from '@/pages/index.module.css';
-
+import {useState} from 'react'
 export default function Home() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log('submit');
   };
+
+  const [durationInBed, setDurationInBed] = useState(0);
+  const [durationAsleep, setDurationAsleep] = useState(0);
+
+  // const [disabled, setDisabled] = useState(true);
 
   return (
     <div className={styles.container}>
@@ -21,10 +26,10 @@ export default function Home() {
         <h1 className="text-2xl">Sleep Behavior</h1>
 
         <form onSubmit={handleSubmit} >
-          <Select label="Duration in bed" />
-          <Select label="Duration asleep" />
+          <Select label="Duration in bed" stateValue={durationInBed} setStateFunction={setDurationInBed} />
+          <Select label="Duration asleep" stateValue={durationAsleep} setStateFunction={setDurationAsleep}/>
 
-          <button type="submit"></button>
+          <button disabled={!(durationInBed && durationAsleep)} type="submit" className='disabled:opacity-50 px-5 py-2 bg-gray-700 text-white'>Submit</button>
         </form>
 
       </main>
